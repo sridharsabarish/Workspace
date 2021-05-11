710c57c7325caef90b739b0c9b55d3772d62eb61
Sabarish Sridhar <sabarish.sr@outlook.com>
2021-05-11 10:24:28 +0530

    May 11, Leetcode

--- 2021/May/Day 11/sumBase.cpp
+++ 2021/May/Day 11/sumBase.cpp
@@ -1,16 +1,14 @@
 // Source : https://leetcode.com/problems/sum-of-digits-in-base-k/submissions/
-class Solution(object):
-    def sumBase(self, n, k):
-        """
-        :type n: int
-        :type k: int
-        :rtype: int
-        """
-        return(self.calculateNumInBase(n,k))
+class Solution {
+public:
+    int sumBase(int n, int k) {
 
-    def calculateNumInBase(self,n,k):
-        digits=[];
-        while(n):
-            digits.append(int(n%k));
-            n = int(n/k)
-        return sum(digits)
+        int sums =0;
+            while(n)
+            {
+                sums +=(n%k);
+                n = n/k;
+            }
+    return sums;
+    }
+};
