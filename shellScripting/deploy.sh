7d8352e3c2e4847ca912a535cef0b056e3011853
spinachisgood <ssridhar@kth.se>
2022-03-16 16:31:20 +0100

    Generated  on

--- shellScripting/deploy.sh
+++ shellScripting/deploy.sh
@@ -2,7 +2,7 @@
 cd ~/projects/Workspace 
 git pull -v
 git add .
-git commit -m "Generated  on 'date' + '%Y-%m-%d %H:%M:%S'";
+git commit -m "Generated  on `date + '%Y-%m-%d %H:%M:%S'`";
 git push
 echo "Successfully pushed to github";
 
