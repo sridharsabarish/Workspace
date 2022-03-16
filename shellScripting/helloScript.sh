e963e06bae9c2a31ab6c17556e1a9b10bfcb87fb
spinachisgood <ssridhar@kth.se>
2022-03-16 16:25:29 +0100

    Generated  on 'date + '%Y-%m-%d %H:%M:%S'

--- shellScripting/helloScript.sh
+++ shellScripting/helloScript.sh
@@ -1,2 +1,22 @@
 #! /bin/bash
-echo " Welcome to bash ";
+#cat >>simple.txt;
+#cat simple.txt;
+: 'This
+is 
+a 
+multi 
+line
+comment'
+
+
+count=11
+
+if [ $count -gt 15 ] # gt, lt, neq
+then
+	echo "Condition is true"
+elif (( $count >10 ))
+then
+	echo " It is greater than 10"
+else
+	echo "Condition is false"
+fi		
