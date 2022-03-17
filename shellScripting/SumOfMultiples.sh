8f18450367a3a1a09c27395552a38cebf3ea1c40
spinachisgood <ssridhar@kth.se>
2022-03-17 12:35:47 +0100

    Generated on, 2022-03-17 12:35:47

--- shellScripting/SumOfMultiples.sh
+++ shellScripting/SumOfMultiples.sh
@@ -4,9 +4,22 @@ sums=0
 while [ $number -lt 1000 ]
 do
 	if [ $(($number % 3)) -eq 0 ]  ||   [ $(($number % 5)) -eq 0 ]
-	then
+	then 
 		sums=$(($sums+$number));
 	fi
 	number=$(( number+1 ))
 done
 echo "Sum of Multiples of 3 or 5 below 1000 : $sums"
+
+
+sums=0
+number=0
+for number in {0..999}
+do
+	if [ $(($number % 3)) -eq 0 ]  ||   [ $(($number % 5)) -eq 0 ]
+	then 
+		sums=$(($sums+$number));
+	fi
+	number=$(( number+1 ))
+done
+echo "Sum of Multiples of 3 or 5 below 1000 (using For Loop) : $sums"
