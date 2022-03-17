400dda730ccf544964770281e9946f28e1ecbd4e
spinachisgood <ssridhar@kth.se>
2022-03-17 12:28:50 +0100

    Generated on, 2022-03-17 12:28:50

--- shellScripting/SumOfMultiples.sh
+++ shellScripting/SumOfMultiples.sh
@@ -3,14 +3,9 @@ number=0
 sums=0
 while [ $number -lt 1000 ]
 do
-	
-	
 	if [ $(($number % 3)) -eq 0 ]  ||   [ $(($number % 5)) -eq 0 ]
 	then
-		#echo "$number"
 		sums=$(($sums+$number));
-		#echo "$sums"
-		
 	fi
 	number=$(( number+1 ))
 done
