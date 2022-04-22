c80b2d4392ed70663c6ef681745f5bdd29a9cce5
Sabarish Sridhar <sabarish.sr@outlook.com>
2022-04-22 20:25:20 +0200

    Coding Practice, 2022-04-22 20:25:20

---
+++ 2022/April/Day 22/SumOfUniqueElements.cpp
@@ -0,0 +1,16 @@
+// Source : https://leetcode.com/problems/sum-of-unique-elements/
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int s=0;
+        for(auto&i:nums)
+        {
+            if(std::count(nums.begin(),nums.end(),i)==1)
+            {
+                s+=i;
+            }
+
+        }
+        return s;
+    }
+};
