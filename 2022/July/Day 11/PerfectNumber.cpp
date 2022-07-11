3a124d1946d8fb24785b7bcdd2c4fe1f31094ba2
Sabarish Sridhar <sabarish.sr@outlook.com>
2022-07-11 11:53:13 +0530

    Coding Practice, 2022-07-11 11:53:13

---
+++ 2022/July/Day 11/PerfectNumber.cpp
@@ -0,0 +1,23 @@
+// Source : https://leetcode.com/problems/perfect-number/submissions/
+// Bad solution can be improvised a lot.
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+
+        int sum=0;
+
+        for(int i=1;i<=num/2;i++)
+        {
+
+            if(num%i==0)
+            {
+                sum+=i;
+                if(sum>num){return false;}
+            }
+         }
+
+        if(sum==num){return true;}
+        return false;
+
+    }
+};
