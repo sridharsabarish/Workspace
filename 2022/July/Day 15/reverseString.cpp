a2120899b5d1d4d1f9fe2ee5bc5a51aa8880cc7a
Sabarish Sridhar <sabarish.sr@outlook.com>
2022-07-15 06:20:41 +0530

    Coding Practice, 2022-07-15 06:20:41

---
+++ 2022/July/Day 15/reverseString.cpp
@@ -0,0 +1,7 @@
+// Source : https://leetcode.com/problems/reverse-string/submissions/
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+       reverse(s.begin(),s.end());
+    }
+};
