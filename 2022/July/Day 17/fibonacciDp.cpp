482a26dbf630a621719576170421f3d262eb5dd9
Sabarish Sridhar <sabarish.sr@outlook.com>
2022-07-17 10:17:59 +0530

    Coding Practice, 2022-07-17 10:17:59

---
+++ 2022/July/Day 17/fibonacciDp.cpp
@@ -0,0 +1,16 @@
+// Source : https://leetcode.com/problems/fibonacci-number/submissions/
+class Solution {
+public:
+    int fib(int n) {
+        int dp[n+1];
+        dp[0]=0;
+        if(n==0){return dp[0];}
+        dp[1]=1;
+        if(n==1){return dp[1];}
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
