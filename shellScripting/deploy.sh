b208687ad2c11f494554457d020ae9fa327999b9
Sabarish Sridhar <sabarish.sr@outlook.com>
2022-07-23 08:35:12 +0530

    Coding Practice, 2022-07-23 08:35:12

--- shellScripting/deploy.sh
+++ shellScripting/deploy.sh
@@ -2,7 +2,13 @@
 cd ~/Projects/Workspace
 git pull -v
 git add .
-git commit -m "Coding Practice, `date +'%Y-%m-%d %H:%M:%S'`";
-git push
-echo "Successfully pushed to github"
+
+if git diff-index --quiet HEAD --; then
+	echo "Nothing to Commit"
+else
+
+	git commit -m "Coding Practice, `date +'%Y-%m-%d %H:%M:%S'`";
+	git push
+	echo "Successfully pushed to github"
+fi
 cd ~
