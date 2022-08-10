66e82cfebbac86a3033239547c5d9ce3463c8e9a
Sabarish Sridhar <sabarish.sr@outlook.com>
2022-08-10 19:29:30 +0200

    Coding Practice, 2022-08-10 19:29:30

---
+++ 2022/August/Day 10/runningSum.cpp
@@ -0,0 +1,16 @@
+// Source : https://leetcode.com/problems/running-sum-of-1d-array/submissions/
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+
+        int sum=0;
+        for(int i=0;i<size(nums);i++)
+        {
+            sum+=nums[i];
+            nums[i]=sum;
+
+        }
+
+        return nums;
+    }
+};
