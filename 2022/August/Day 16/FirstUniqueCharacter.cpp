4c8278ea9e41a8ade018f78db12deda4ed0ef057
Sabarish Sridhar <sabarish.sr@outlook.com>
2022-08-16 07:24:01 +0200

    Coding Practice, 2022-08-16 07:24:01

---
+++ 2022/August/Day 16/FirstUniqueCharacter.cpp
@@ -0,0 +1,18 @@
+// Source : https://leetcode.com/problems/first-unique-character-in-a-string/submissions/
+class Solution {
+public:
+    int firstUniqChar(string s) {
+
+        map<char,int> m;
+        for(auto&i:s)
+        {m[i]+=1;
+        }
+        for(int i=0;i<size(s);i++)
+        {if(m[s[i]]==1)
+
+            return i;
+        }
+
+        return -1;
+    }
+};
