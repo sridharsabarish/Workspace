3ce58576798145a3d726179b5253adb08574c718
Sabarish Sridhar <sabarish.sr@outlook.com>
2022-08-30 14:59:05 +0200

    Coding Practice, 2022-08-30 14:59:05

--- 2022/August/Day 30/rotateImage.cpp
+++ 2022/August/Day 30/rotateImage.cpp
@@ -1,11 +1,11 @@
+/*
+ Source : https://leetcode.com/problems/rotate-image/submissions/
+ Logic : Row[i] becomes column[n-i-1]
+
+ Solution Quality : Run time : 0 ms, memory 7.2 MB
+
+ */
 class Solution {
-   /*
-    Source : https://leetcode.com/problems/rotate-image/submissions/
-    Logic : Row[i] becomes column[n-i-1]
-
-    Solution Quality : Run time : 0 ms, memory 7.2 MB
-
-    */
 public:
     void rotate(vector<vector<int>>& matrix) {
 
