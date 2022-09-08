b48447f43d49667dc5c3d4b34cdf45020e50734f
Sabarish Sridhar <sabarish.sr@outlook.com>
2022-09-08 07:18:22 +0200

    Coding Practice, 2022-09-08 07:18:22

---
+++ 2022/September/Day 8/binaryTreeTraversals.cpp
@@ -0,0 +1,52 @@
+/* Source : https://leetcode.com/problems/binary-tree-inorder-traversal/
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+
+    vector<int> vec;
+    vector<int> inorderTraversal(TreeNode* root) {
+
+        if(root==NULL){return {};}
+        inorderTraversal(root->left);
+        vec.insert(vec.end(),root->val);
+        inorderTraversal(root->right);
+        return vec;
+
+
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+
+        if(root==NULL)
+        {return {};}
+        vec.push_back(root->val);
+        preorderTraversal(root->left);
+        preorderTraversal(root->right);
+        return vec;
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+
+        if(root==NULL){return {};}
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);
+        vec.insert(vec.end(),root->val);
+        return vec;
+
+    }
+
+
+};
