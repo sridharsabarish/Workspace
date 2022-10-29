aecdae63edfbcbdd33b69d6e174851a1f433ddd3
Sabarish Sridhar <sabarish.sr@outlook.com>
2022-10-29 07:26:23 +0200

    Coding Practice, 2022-10-29 07:26:23

--- 2022/Oktober/Day 29/singleNumber.cpp
+++ 2022/Oktober/Day 29/singleNumber.cpp
@@ -1,4 +1,32 @@
 // Source :  https://leetcode.com/problems/single-number/submissions/
+// Better  solution
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+        sort(nums.begin(),nums.end());
+        if(size(nums)==1){return nums[0];}
+        if(nums[0]!=nums[1]){return nums[0];}
+        if(nums[size(nums)-1]!=nums[size(nums)-2]){return nums[size(nums)-1];}
+
+        for(int i=1;i<size(nums)-1;i++)
+        {
+
+            if((nums[i]!=nums[i-1]) && (nums[i]!=nums[i+1]))
+            {
+                return nums[i];
+            }
+
+        }
+        return -1;
+    }
+};
+
+
+
+
+
+// Source :  https://leetcode.com/problems/single-number/submissions/
 // Lazy solution
 class Solution {
 public:
