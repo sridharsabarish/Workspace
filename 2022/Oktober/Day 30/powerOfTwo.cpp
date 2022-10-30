7ea8a75e1281778131cf865a3db3b4f1a7c7e6c5
Sabarish Sridhar <sabarish.sr@outlook.com>
2022-10-30 07:36:35 +0100

    Coding Practice, 2022-10-30 07:36:35

--- 2022/Oktober/Day 30/powerOfTwo.cpp
+++ 2022/Oktober/Day 30/powerOfTwo.cpp
@@ -16,3 +16,47 @@ public:
         return true;
     }
 };
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+
+        if(n<=0){return false;}
+
+        if(n==1){return true;}
+
+        while(n)
+        {
+            if(n!=1 && n%3!=0)
+            {
+                return false;
+            }
+            n/=3;
+        }
+        return true;
+    }
+};
+
+
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+
+        if(n<=0){return false;}
+
+        if(n==1){return true;}
+
+        while(n)
+        {
+            if(n!=1 && n%4!=0)
+            {
+                return false;
+            }
+            n/=4;
+        }
+        return true;
+
+
+    }
+};
