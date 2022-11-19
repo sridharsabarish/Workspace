c4fa45441740bf8bd4783ff632cdc40db9588b0d
Sabarish Sridhar <sabarish.sr@outlook.com>
2022-11-19 07:24:22 +0100

    Coding Practice, 2022-11-19 07:24:22

---
+++ 2022/November/Day 19/subtractProdAndSumOfInteger.cpp
@@ -0,0 +1,20 @@
+// Source : https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/submissions/
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+
+
+        int sum=0;
+        int product=1;
+
+        while(n)
+        {
+            sum+=n%10;
+            product*=n%10;
+            n/=10;
+        }
+
+        return product-sum;
+
+    }
+};
