1e0fd6ed02a65e71cf9129d5073cd119fcd394b5
Sabarish Sridhar <sabarish.sr@outlook.com>
2022-11-21 18:56:52 +0100

    Coding Practice, 2022-11-21 18:56:52

---
+++ 2022/November/Day 21/ConvertTemperature.cpp
@@ -0,0 +1,7 @@
+// Source : https://leetcode.com/problems/convert-the-temperature/submissions/
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        return {celsius+273.15, celsius*1.80 + 32.00};
+    }
+};
