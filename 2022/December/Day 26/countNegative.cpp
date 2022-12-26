83a5455c78cccd004772c1c07e84fda0a70326b0
Sabarish Sridhar <sabarish.sr@outlook.com>
2022-12-26 06:54:18 +0100

    Coding Practice, 2022-12-26 06:54:18

---
+++ 2022/December/Day 26/countNegative.cpp
@@ -0,0 +1,28 @@
+// Source : https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        
+        
+        int imin=0;
+        int jmin=0;
+        int count=0;
+        for(int i=imin;i<size(grid);i++)
+        {
+            jmin=0;
+            for(int j=jmin;j<size(grid[0]);j++)
+            {
+                if(grid[i][j]<0)
+                {
+                    count++;
+                    imin=i;
+                    jmin=j;
+                }
+                
+            }
+        }
+        return count;
+        
+        
+    }
+};
