a2c1f20dce73f18161b86d732705f31d3ae9f48b
Sabarish Sridhar <sabarish.sr@outlook.com>
2023-03-04 07:35:36 +0100

    Coding Practice, 2023-03-04 07:35:36

---
+++ 2023/March/Day 4/incompleteVisitrooms.cpp
@@ -0,0 +1,22 @@
+// Incomplete
+// Mind Freeze : https://leetcode.com/problems/keys-and-rooms/
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        
+        int size=size(rooms);
+        
+        int visited=0;
+        
+        vector<int> to_visit =rooms[0];
+        visited+=size(rooms[0]);
+       while(size(to_visit)!=0)
+        {
+            
+
+            
+        }
+        
+        
+    }
+};
