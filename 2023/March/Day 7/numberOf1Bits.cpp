09b3f46b1bb88bd6a7f0880c6a8e6053ac61192d
Sabarish Sridhar <sabarish.sr@outlook.com>
2023-03-07 20:15:15 +0100

    Coding Practice, 2023-03-07 20:15:15

---
+++ 2023/March/Day 7/numberOf1Bits.cpp
@@ -0,0 +1,17 @@
+// Source : https://leetcode.com/problems/number-of-1-bits/submissions/
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count=0;
+        while(n)
+        {
+            if(n&1)
+            {
+                count++;
+            }
+            n=n>>1;
+        }
+        
+        return count;
+    }
+};
