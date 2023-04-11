694de5bb662addffe6ec0a2da9c6416d893ec8cd
Sabarish Sridhar <sabarish.sr@outlook.com>
2023-04-11 20:04:11 +0200

    Coding Practice, 2023-04-11 20:04:11

---
+++ 2023/April/Day 11/IncompleteAddString.cpp
@@ -0,0 +1,8 @@
+// Source : https://leetcode.com/problems/add-strings/submissions/
+// Just for Habit/ Incomplete
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+       return to_string(stoi(num1)+stoi(num2));
+    }
+};
