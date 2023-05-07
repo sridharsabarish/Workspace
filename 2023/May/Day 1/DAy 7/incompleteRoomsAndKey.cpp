9e77b1948854c800e8e9ad41ca405c20c29c42e5
Sabarish Sridhar <sabarish.sr@outlook.com>
2023-05-07 06:50:15 +0200

    Coding Practice, 2023-05-07 06:50:15

---
+++ 2023/May/Day 1/DAy 7/incompleteRoomsAndKey.cpp
@@ -0,0 +1,38 @@
+/*
+
+Go through each key of the map and see if it has a value (i.e room's key)
+Add the found keys to visited list.
+if(size(visited)==size(rooms))then we say, we can visit all keys.
+
+Seems more like FSM. How do you know if you can exit an FSM.
+
+Status : Incomplete
+*/
+
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        
+        set<int> visited v;
+        
+        
+        for(auto &i:rooms)
+        {
+            
+            
+           loopThrough(rooms[index])
+               
+        }
+        
+        
+        
+        
+    }
+    
+    vector<int> loopThrough(<vector<int>& room,vector<int> visited)
+    {
+        
+        
+        
+    }
+};
