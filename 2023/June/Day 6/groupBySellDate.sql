bbe185d95d1b5917061b3c7bacb34139e021905c
Sabarish Sridhar <sabarish.sr@outlook.com>
2023-06-07 19:01:44 +0200

    Coding Practice, 2023-06-07 19:01:44

--- 2023/June/Day 6/groupBySellDate.sql
+++ 2023/June/Day 6/groupBySellDate.sql
@@ -1,3 +1,5 @@
+# Source : https://leetcode.com/problems/group-sold-products-by-the-date/
+# Learned about group concat
 # Write your MySQL query statement below
 SELECT sell_date,count(DISTINCT product) as num_sold,
 GROUP_CONCAT( DISTINCT product order by product ASC separator ',' ) as products
