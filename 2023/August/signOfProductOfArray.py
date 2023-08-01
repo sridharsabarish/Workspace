8c6c8943a35fb3a95d20a909eee4ea1c357cc735
Sabarish Sridhar <sabarish.sr@outlook.com>
2023-08-01 06:47:37 +0200

    Coding Practice, 2023-08-01 06:47:37

---
+++ 2023/August/signOfProductOfArray.py
@@ -0,0 +1,16 @@
+# Source : https://leetcode.com/problems/sign-of-the-product-of-an-array/
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+
+        neg=0;
+        zero=0;
+
+        for i in nums:
+            if i<0:
+                neg=neg+1
+            if i==0:
+                zero=1;
+                return 0;
+        if(neg%2==0):
+            return 1
+        return -1;
