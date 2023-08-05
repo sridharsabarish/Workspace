e804bda17d40ede514b3196cd717aa8e476e7932
Sabarish Sridhar <sabarish.sr@outlook.com>
2023-08-05 19:40:09 +0200

    Coding Practice, 2023-08-05 19:40:09

--- 2023/August/Day 4/fibonacciDP.cpp
+++ 2023/August/Day 4/fibonacciDP.cpp
@@ -24,18 +24,33 @@ cout<<endl;
 }
 
 
+/*
+To do : Generate Fibonacci using Top Down Approach, with and without memoization;
+Need to refine it to store it somewhere and print.
+*/
+
+int generateFibTopDown(int n)
+{
+
+    if(n==2 || n==1)
+    return 1;
+
+    return generateFibTopDown(n-1)+generateFibTopDown(n-2);
+}
+
+
 
 
 int main()
 {
 
 
-for(int i=3;i<30;i++)
-{
-generateFibBottomUp(i);
-}
-//generateFibBottomUp(5);
-//generateFibBottomUp(10);
+// for(int i=3;i<30;i++)
+// {
+// generateFibBottomUp(i);
+// }
+cout<<generateFibTopDown(10)<<" \n";
+generateFibBottomUp(10);
 //generateFibBottomUp(20);
 return 0;
 
