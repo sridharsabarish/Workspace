765c31546735993da6826695ccacf0a68cc4919a
Sabarish Sridhar <sabarish.sr@outlook.com>
2023-08-13 07:03:20 +0200

    Coding Practice, 2023-08-13 07:03:19

---
+++ 2023/August/Day 13/averageValueOfEvenNumbers.py
@@ -0,0 +1,13 @@
+# Source : https://leetcode.com/problems/average-value-of-even-numbers-that-are-divisible-by-three/submissions/
+class Solution:
+    def averageValue(self, nums: List[int]) -> int:
+        i=0;
+        sums=0;
+        for number in nums:
+            if(number%6==0):
+                sums=sums+number
+                i=i+1
+        if(i!=0):
+            return int(sums/i)
+        return 0
+        
