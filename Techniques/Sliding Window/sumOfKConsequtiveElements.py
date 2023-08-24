6687163db3d87c5f11f2f2c010c36fdc21cff2b6
Sabarish Sridhar <sabarish.sr@outlook.com>
2023-08-24 06:59:26 +0200

    Coding Practice, 2023-08-24 06:59:26

--- Techniques/Sliding Window/sumOfKConsequtiveElements.py
+++ Techniques/Sliding Window/sumOfKConsequtiveElements.py
@@ -6,8 +6,8 @@ def bruteForce(arr,k,n):
     for i in range(n-k+1):
         current_sum=0
         for j in range(k):
-            current_sum = current_sum + arr[i][j]
-        nax_sum=max(current_sum,max_sum)
+            current_sum = current_sum + arr[i+j]
+        max_sum=max(current_sum,max_sum)
     return max_sum
 
 def slidingWindow(arr,k,n):
@@ -22,23 +22,59 @@ def slidingWindow(arr,k,n):
     return max_sum
 
 
-def testcase1():
-    arr=[1,2,3,4,5,6,7,8,9,10,11,12,13];
-    return arr,3,4
-
-def main():
-    arr,n,k=testcase1
+def executeTest(arr,n,k):
     start=time.time();
     out_brute=bruteForce(arr,n,k)
     end=time.time();
     out_sliding=slidingWindow(arr,n,k);
     finaltime=time.time();
-    
     print("Value from brute force : ", out_brute)
     print(" Value from sliding window : ",out_sliding )
     print("Time brute (ms) :",end-start)
     print("Time sliding (ms) : ",finaltime-end)
+    print("********** End of Test ***********")
 
-main()
+
+# Todo : Read from file instead of functions for Tests
+
+def testcase1():
+    arr=[1,2,3,4,5,6,7,8,9,10,11,12,13];
+    return arr,5,len(arr)
+
+def testcase2():
+    arr=[5,10,15,12231,1,2,4,213,2,3,4,5,6,1,2,3,5,6,2];
+    return arr,4,len(arr)
+
+def testcase3():
+    arr=[1,2,3,4,5,6,7,8,9,10,11,12,13];
+    return arr,3,len(arr)
+
+def testcase4():
+    arr=[1,2,3,4,5,6,7,8,9,10,11,12,13];
+    return arr,6,len(arr)
+
+def testcase5():
+    arr=[1,2,3,4,5,6,7,8,9,10,11,12,13];
+    return arr,5,len(arr)
+
+
+def main():
+    print(" Sliding Window")
+    print("First test") 
+    arr,k,n=testcase1()
+    executeTest(arr,k,n)
+    print("Second test") 
+    arr,k,n=testcase2()
+    executeTest(arr,k,n)
+    print("Third test") 
+    arr,k,n=testcase3()
+    executeTest(arr,k,n)
+    print("Fourth test") 
+    arr,k,n=testcase4()
+    executeTest(arr,k,n)
+    print("Fifth test") 
+    arr,k,n=testcase5()
+    executeTest(arr,k,n)
+main();
 
     
