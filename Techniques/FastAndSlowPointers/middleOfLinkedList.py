cd7279e7f4dc76efe5c2fdbf615c6f679cb0fb7f
Sabarish Sridhar <sabarish.sr@outlook.com>
2023-08-29 07:24:56 +0200

    Coding Practice, 2023-08-29 07:24:56

---
+++ Techniques/FastAndSlowPointers/middleOfLinkedList.py
@@ -0,0 +1,24 @@
+'''
+
+Middle of Linked List
+
+1) Use two pointers fast and slow. 
+2) Fast moves 2 pace, Slow 1 pace. 
+3) When Fast reaches end, Slow will be at the middle of Linked List.
+
+
+'''
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow=head;
+        fast=head
+        while(fast!=None and fast.next!=None):
+            slow=slow.next
+            fast=fast.next.next;
+        return slow
+    
