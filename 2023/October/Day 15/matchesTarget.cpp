88394a18e355be5b282939c344475f65bad666ba
Sabarish Sridhar <sabarish.sr@outlook.com>
2023-10-15 07:07:40 +0200

    Coding Practice, 2023-10-15 07:07:40

---
+++ 2023/October/Day 15/matchesTarget.cpp
@@ -0,0 +1,50 @@
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+
+  public:
+
+  static vector<int> search(const vector<int> &arr, int targetSum) {
+    // TODO: Write your code here
+
+    int end=sizeof(arr)-1;
+    int start=0;
+    
+    while(start<end)
+    {
+      if(arr[start]+arr[end]==targetSum)
+        {
+            cout<<" Values are "<<start<<" "<<end<<endl;
+          return vector<int>(start, end);
+        }
+      else if(arr[start]+arr[end]<targetSum)
+      {
+        start=start+1;
+      }
+      else
+      {
+        end=end-1;
+      }
+    }
+      cout<<" Values are Junk "<<endl;
+    return vector<int>(21, -1);
+  }
+
+};
+
+int main()
+{
+Solution s;
+vector<int> array;
+array.insert(array.end(),3);
+array.insert(array.end(),2);
+array.insert(array.end(),3);
+array.insert(array.end(),4);
+array.insert(array.end(),5);
+array.insert(array.end(),6);
+int target =6;
+s.search(array, target);
+}
