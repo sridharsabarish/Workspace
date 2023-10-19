5911d4d32fde79ad4612b2ffdd3141d4effedfbf
Sabarish Sridhar <sabarish.sr@outlook.com>
2023-10-19 07:39:20 +0200

    Coding Practice, 2023-10-19 07:39:20

--- 2023/October/Day 17/IncompleteFruitsInABasket.cpp
+++ 2023/October/Day 17/IncompleteFruitsInABasket.cpp
@@ -1,76 +1,94 @@
-/* 
+// Start
+// Curr : this keeeps growing.
 
-* A farm with trees from left to right
-* Trees represented by integer array fruits, with fruits[i] the type.
-* Collect as much fruit as possible // DP?
-* Two baskets, each hold single type of fruit., no limit to amount of fruit
-* 1 fruit, right.
-* Two types of fruits we can have, one in each basket.
+// Keep track of dictionary with fruit type. -> Maybe not needed
+// if current is different, we remove first element,
 
-* continuous but see where it can be added.
+/*
+[1,2,2,1,1,3,3,3,3,3,1]
+4
+
+then 3 comes along
+So we need to keep track of max
+We remove the element that was not prev i.e 2 in our case
+and we move to the index where we had prev as start;
 
 
 
-* This is a sliding window problem.
-* We need a mechanism that removes one type of fruit, previous one perhaps and see what happens when you add the next one.
 
 
 */
 
 
-
-
-
 class Solution {
 public:
-
-    bool fruitDoesNotExistsInCollection(vector<int> collectedFruits, vector<int>& fruits, i) 
+    int first_occurence_previous(vector<int>& fruits,int prev,int curr)
     {
-
-        return std::find(collectedFruits.begin(),collectedFruits.end(),fruits[i])==collectedFruits.end()
-    }
-
-    int totalFruit(vector<int>& fruits) {
-    
-        vector<int> collectedFruits;
-        map<int,int> m;
-        int max=0;
-        start=0;
-        i=0;
-        int prevType=-1;
-        
-        while(start<fruits.size())
+        int detected=1;
+        while(detected)
         {
-            
-            if(collectedFruits.size() <=2)
+            if(fruits[curr]==prev)
             {
-                if(fruitDoesNotExistsInCollection(collectedFruits,fruits,fruits[i]))
-                {
-                    collectedFruits.insert(collectedFruits.end(),fruits[i]);
-                }
-            prevType=Fruits[i];
+                curr--;
+            }
+            else {
+                detected=0;
+                return curr;
+            }
+        }
+        return -1;
+    }
+    int totalFruit(vector<int>& fruits) {
+        int max_size=0; 
+        int start=0;
+        int curr=-1;
+        int prev=-1;
+
+        vector<int> fruitTypes;
+
+        for(int i=0;i<fruits.size();i++)
+        {
+            curr=curr+1;
+            //cout<<" "<<fruits[i]<<" ";
+            if(std::find(fruitTypes.begin(),fruitTypes.end(),fruits[i])==fruitTypes.end() && fruitTypes.size()<2)
+            {
+                fruitTypes.insert(fruitTypes.end(),fruits[i]);
+            }
+
+            else if(std::find(fruitTypes.begin(),fruitTypes.end(),fruits[i])!=fruitTypes.end())
+            {
+                continue;
             }
 
             else
             {
-                if(!fruitDoesNotExistsInCollection(collectedFruits,fruits,fruits[i]))
+                for(int i=0;i<2;i++)
                 {
-                    
+                    if(fruitTypes[i]!=prev)
+                    {
+                        remove(fruitTypes.begin(),fruitTypes.end(),fruitTypes[i]);
+                    }
                 }
 
+                fruitTypes.insert(fruitTypes.end(),fruits[i]);
+                //cout<<"\n  new fruit type :"<<fruits[i]<<"setting start to "<<start+1<<endl;
+                start=first_occurence_previous(fruits,prev,curr);
+                
+
+
             }
+            
+            max_size=((curr-start)+1)>max_size?((curr-start)+1):max_size;
 
-            m[collectedFruits[i]]+=1;
-            i++;
+            prev = fruits[i];
         }
+       //cout<<"\n Curr : "<<curr<<" Start : "<<start<<" max_size : "<<max_size;
 
-
-
-
-    
-
-
-
+       if(curr-start+1>max_size)
+       {
+           return curr-start+1;
+       }
+       return max_size+1;
 
 
     }
