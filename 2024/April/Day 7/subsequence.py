72154728d481f26a107b716a75c25c457d0b7ce1
Sabarish Sridhar <sabarish.sr@outlook.com>
2024-04-07 07:31:33 +0200

    Coding Practice, 2024-04-07 07:31:33

--- 2024/April/Day 7/subsequence.py
+++ 2024/April/Day 7/subsequence.py
@@ -1,1 +1,31 @@
 
+
+'''
+Method 1
+Pick and Don't Pick 
+
+
+if(Inp==0)
+return output
+
+else:
+
+print(input[1:], output+input[0])
+print(input[1:], output)
+
+
+
+'''
+def subsequence(inp,out):
+    if len(inp)==0:
+        print(out,end=' ')
+        return 
+    subsequence(inp[1:],out+inp[0])
+    subsequence(inp[1:],out)
+    
+ out = ""
+ input="abcd"   
+    
+    
+    
+    
